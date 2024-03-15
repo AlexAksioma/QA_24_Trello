01c7eb0d5c1b06e595c870ea2d7fb9c88d83a4cd
AlexAksioma <alexmedqwerty@gmail.com>
2024-03-15 11:17:10 +0200

    added method loginPositiveTest+assert

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,15 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
 
     @Test
     public void loginPositiveTest(){
-        System.out.println("==============");
+        String email = "aksiomamedved@gmail.com";
+        String password = "AlexMed123!";
+        app.getHelperUser().login(email, password);
+        Assert.assertTrue(app.getHelperUser().isElementPresent_btnAccount());
     }
 }
