d9f4e3f202b693e5cd752f874245ecd0b5a9651e
AlexAksioma <alexmedqwerty@gmail.com>
2024-03-19 19:55:57 +0200

    added positive, negative boards test

--- src/test/java/tests/BoardsTests.java
+++ src/test/java/tests/BoardsTests.java
@@ -1,8 +1,11 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.Random;
 
 public class BoardsTests extends TestBase{
 
@@ -13,6 +16,23 @@ public class BoardsTests extends TestBase{
 
     @Test
     public void createNewBoardPositiveTest(){
+        int i = new Random().nextInt(1000);
+        String boardTitle = "qa24_"+i;
+        app.getHelperBoards().createNewBoard(boardTitle);
+        Assert.assertTrue(app.getHelperBoards().isTextInElementEquals_boardTitle(boardTitle));
+    }
 
+    @Test
+    public void createNewBoardNegativeTest_EmptyBoardTitle(){
+        app.getHelperBoards().createNewBoard("  ");
+        Assert.assertTrue(app.getHelperBoards().isElementPresent_inputBoardTitle());
+    }
+
+    @AfterMethod
+    public void afterTest(){
+        if(app.getHelperBoards().isElementPresent_boardTitle())
+            app.getHelperBoards().clickBtnBoards();
+        if(app.getHelperBoards().isElementPresent_inputBoardTitle())
+            app.getHelperBoards().clickBtnCloseCreateBoardForm();
     }
 }
